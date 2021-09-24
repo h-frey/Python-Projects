6026de49db7b0edcd66b221a5085dbcbdc8c07dd
Humphrey <hnyanzi32@gmail.com>
2021-09-24 22:26:18 +0300

    Addition of the CS50  population script

---
+++ marks_test.py
@@ -0,0 +1,6 @@
+answers = []
+for i in range(31):
+    x = ((i / 30) * 100)
+    answers.append(round(x,0))
+
+print(answers)
---
+++ population.py
@@ -0,0 +1,17 @@
+def calc(start_size, end_size, years = 0):
+    while start_size < end_size:
+        start_size = (start_size + (start_size//3) - (start_size//4))
+        years += 1
+        continue
+    return years
+while True:
+
+    start_size = int(input("Start size: "))
+    end_size =int(input("End size: "))
+
+    if (start_size < 9) or (end_size < start_size):
+        continue
+    
+    else:
+        print("Years: {}".format(calc(start_size, end_size)))
+        break
